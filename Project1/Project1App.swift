7a19285b227c42b8f7e8f124c27b49b0dd1315e2
David Ferreira <81036027+DavidFerrs@users.noreply.github.com>
2024-10-26 16:54:59 -0300

    first commit

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by David Ferreira on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            TextFieldView()
+        }
+    }
+}
---
+++ Project1/TextFieldView.swift
@@ -0,0 +1,84 @@
+//
+//  TextFieldView.swift
+//  MyApp
+//
+//  Created by David Ferreira on 26/10/24.
+//
+
+import SwiftUI
+
+struct TextFieldView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocus: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var tipValue: Double {
+        let tipSelection = Double(tipPercentage)
+        
+        return checkAmount / 100 * tipSelection
+    }
+        
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        
+        let tipValue = tipValue
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson =  grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format:  .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocus)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0..<101){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Total amount plus tip") {
+                    Text("\(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) + \(tipValue, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) = \(checkAmount + tipValue, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))")
+                }
+                
+            }
+            .navigationTitle("WeSplit")
+            .toolbar{
+                if amountIsFocus {
+                    Button("Done"){
+                        amountIsFocus = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    TextFieldView()
+}
